e0db1896676e157daed63253296066eb19d4288f
Baptiste Langlade <baptouuuu@gmail.com>
2019-10-13 12:12:05 +0200

    add library artist model

---
+++ src/SDK/Library/Artist.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+
+namespace MusicCompanion\AppleMusic\SDK\Library;
+
+use MusicCompanion\AppleMusic\SDK\Library\Artist\{
+    Id,
+    Name,
+};
+
+final class Artist
+{
+    private $id;
+    private $name;
+
+    public function __construct(Id $id, Name $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function id(): Id
+    {
+        return $this->id;
+    }
+
+    public function name(): Name
+    {
+        return $this->name;
+    }
+}
