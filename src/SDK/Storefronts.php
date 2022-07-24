a5e7ba106f1e1d3dbf838d518389b776717cd5bf
Baptiste Langlade <baptouuuu@gmail.com>
2022-07-24 16:30:20 +0200

    return an empty storefront set instead of throwing

--- src/SDK/Storefronts.php
+++ src/SDK/Storefronts.php
@@ -6,6 +6,7 @@ namespace MusicCompanion\AppleMusic\SDK;
 use Innmind\Http\{
     Header,
     Message\Request\Request,
+    Message\Response,
     Message\Method,
     ProtocolVersion,
     Headers,
@@ -30,19 +31,44 @@ final class Storefronts
      */
     public function all(): Set
     {
-        /** @psalm-suppress MixedArgumentTypeCoercion */
-        $response = ($this->fulfill)(new Request(
+        return ($this->fulfill)(new Request(
             Url::of('/v1/storefronts'),
             Method::get,
             ProtocolVersion::v20,
             Headers::of(
                 $this->authorization,
             ),
-        ))->match(
-            static fn($response) => $response,
-            static fn() => throw new \RuntimeException,
-        );
+        ))
+            ->map($this->decode(...))
+            ->map(static fn($storefronts) => Set::of(...$storefronts['data']))
+            ->map(static fn($storefronts) => $storefronts->map(
+                static fn($storefront) => new Storefront(
+                    new Storefront\Id($storefront['id']),
+                    new Storefront\Name($storefront['attributes']['name']),
+                    Storefront\Language::of($storefront['attributes']['defaultLanguageTag']),
+                    Set::of(...$storefront['attributes']['supportedLanguageTags'])->map(Storefront\Language::of(...)),
+                ),
+            ))
+            ->match(
+                static fn($storefronts) => $storefronts,
+                static fn() => Set::of(),
+            );
+    }
 
+    /**
+     * @return array{
+     *     data: list<array{
+     *         id: string,
+     *         attributes: array{
+     *             name: string,
+     *             defaultLanguageTag: string,
+     *             supportedLanguageTags: list<string>
+     *         }
+     *     }>
+     * }
+     */
+    private function decode(Response $response): array
+    {
         /**
          * @var array{
          *     data: list<array{
@@ -55,19 +81,6 @@ final class Storefronts
          *     }>
          * }
          */
-        $resource = Json::decode($response->body()->toString());
-        /** @var Set<Storefront> */
-        $storefronts = Set::of();
-
-        foreach ($resource['data'] as $storefront) {
-            $storefronts = ($storefronts)(new Storefront(
-                new Storefront\Id($storefront['id']),
-                new Storefront\Name($storefront['attributes']['name']),
-                Storefront\Language::of($storefront['attributes']['defaultLanguageTag']),
-                Set::of(...$storefront['attributes']['supportedLanguageTags'])->map(Storefront\Language::of(...)),
-            ));
-        }
-
-        return $storefronts;
+        return Json::decode($response->body()->toString());
     }
 }
